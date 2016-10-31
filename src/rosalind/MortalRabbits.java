7591b54e36c2c1718003e5fe36b1e4332d00255c
KingMathCS <kingmathcs@gmail.com>
2016-10-30 21:37:26 -0700

    mortal rabbits

--- src/rosalind/MortalRabbits.java
+++ src/rosalind/MortalRabbits.java
@@ -27,14 +27,14 @@ public class MortalRabbits
 	{
 		int n = nums(file)[0];
 		int m = nums(file)[1];
-		
+
 		BigInteger[] rabbitAges = new BigInteger[m];
 		BigInteger[] holder = new BigInteger[m];
-		
+
 		Arrays.fill(rabbitAges, BigInteger.ZERO);
 		Arrays.fill(holder, BigInteger.ZERO);
 		rabbitAges[0] = BigInteger.ONE;
-		
+
 		for(int month = 2; month <= n; month++)
 		{
 			for(int i = 0; i < rabbitAges.length; i++)
@@ -43,42 +43,42 @@ public class MortalRabbits
 				{
 					holder[0] = holder[0].add(rabbitAges[i]);
 				}
-				
+
 				if(i != rabbitAges.length - 1)
 				{
 					holder[i + 1] = holder[i + 1].add(rabbitAges[i]);
 				}
 			}
-			
+
 			System.arraycopy(holder, 0, rabbitAges, 0, holder.length);
 			Arrays.fill(holder, BigInteger.ZERO);
-			
+
 			System.out.println(month);
 		}
-		
+
 		BigInteger numRabbits = BigInteger.ZERO;
-		
+
 		for(int i = 0; i < rabbitAges.length; i++)
 		{
 			numRabbits = numRabbits.add(rabbitAges[i]);
 		}
-		
+
 		System.out.println(numRabbits);
 	}
-	
+
 	// returns two integers from file in an integer array
-    public static int[] nums(String file) throws IOException
-    {
-        Scanner inFile = new Scanner(new File(file));
-        int[] nums = new int[2];
-        int i = 0;
+	public static int[] nums(String file) throws IOException
+	{
+		Scanner inFile = new Scanner(new File(file));
+		int[] nums = new int[2];
+		int i = 0;
 
-        while(inFile.hasNext())
-        {
-            nums[i++] = inFile.nextInt();
-        }
-        inFile.close();
+		while(inFile.hasNext())
+		{
+			nums[i++] = inFile.nextInt();
+		}
+		inFile.close();
 
-        return nums;
-    }
+		return nums;
+	}
 }
